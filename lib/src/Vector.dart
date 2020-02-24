670b006ba69aef52d1ff182d6f971184b2d01c93
Mehmet Hakan Satman <mhsatman@gmail.com>
2020-02-24 11:29:55 +0300

    documentation

--- lib/src/Vector.dart
+++ lib/src/Vector.dart
@@ -1,4 +1,5 @@
 class Vector {
+  /// Static method for vector summation. 
   static List<double> Sum(List<double> v1, List<double> v2) {
     List<double> result = List<double>(v1.length);
     for (int i = 0; i < v1.length; i++) {
@@ -6,7 +7,7 @@ class Vector {
     }
     return result;
   }
-
+  /// Static method for vector subtraction
   static List<double> Sub(List<double> v1, List<double> v2) {
     List<double> result = List<double>(v1.length);
     for (int i = 0; i < v1.length; i++) {
@@ -14,7 +15,7 @@ class Vector {
     }
     return result;
   }
-
+  /// Static method to multiply all elements of [v1] with a [scaler]. 
   static List<double> ProdWithScaler(List<double> v1, double scaler) {
     List<double> result = List<double>(v1.length);
     for (int i = 0; i < v1.length; i++) {
